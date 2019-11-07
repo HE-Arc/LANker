ca654dfaf26af210931cc3308c57a377fd3e6f2f
artan.sadiku <artan.sadiku@he-arc.ch>
2019-11-07 14:30:46 +0100

    Deleted unused middleware ForceSSL. The forcing has been moved to AppServiceProvider.php

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -61,7 +61,6 @@ class Kernel extends HttpKernel
         'signed' => \Illuminate\Routing\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
-        'forcessl' => \App\Http\Middleware\ForceSSL::class,
     ];
 
     /**
