0f299b60a1b1f8bfb09c9f245b7837eb370716d5
artan.sadiku <artan.sadiku@he-arc.ch>
2019-11-09 20:56:23 +0100

    Added description password, email and description edit.

--- routes/web.php
+++ routes/web.php
@@ -25,30 +25,33 @@ Route::get('/event', function() {
 });
 
 // TODO: ajouter vue quand user pas trouvé (plutot que redirect à home)
-// TODO: the logic need to be in a controller
-// TODO: don't except a name, give the function the user himself as an object
 Route::get('/profile/{name}',[
   'as' => 'profile',
   'uses' => 'UserController@show']);
 
 Route::get('/profile/edit/{user}',[
   'as' => 'edit_profile',
-  'uses' => 'UserController@edit'])->middleware('auth');
+  'uses' => 'UserController@edit'
+])->middleware('auth');
 
 Route::patch('profile/edit/{user}/update', [
   'as' => 'update_profile',
-  'uses' => 'UserController@update'])->middleware('auth');
+  'uses' => 'UserController@update'
+])->middleware('auth');
 
 Route::delete('profile/delete/{user}', [
   'as' => 'delete_profile',
-  'uses' => 'UserController@delete'])->middleware('auth');
+  'uses' => 'UserController@delete'
+])->middleware('auth');
 
 // Debug route to restore all softDeleted users
 Route::get('restore', [
   'as' => 'restore_profiles',
-  'uses' => 'UserController@restore']);
+  'uses' => 'UserController@restore'
+]);
 
 // Debug route to permanently delete all softDeleted users
 Route::get('forceDelete', [
   'as' => 'forceDelete_profiles',
-  'uses' => 'UserController@forceDelete']);
+  'uses' => 'UserController@forceDelete'
+]);
