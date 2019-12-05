ec37e57cafe357abedb898e7989cae1971a7973c
artan.sadiku <artan.sadiku@he-arc.ch>
2019-12-05 14:13:33 +0100

    Added games to form.

--- public/js/taginput.js
+++ public/js/taginput.js
@@ -1,43 +1,99 @@
-// $('#game').autocomplete({
-//   source: []
-// })
+// var nhlTeams = ['Anaheim Ducks', 'Atlanta Thrashers', 'Boston Bruins', 'Buffalo Sabres', 'Calgary Flames', 'Carolina Hurricanes', 'Chicago Blackhawks', 'Colorado Avalanche', 'Columbus Blue Jackets', 'Dallas Stars', 'Detroit Red Wings', 'Edmonton OIlers', 'Florida Panthers', 'Los Angeles Kings', 'Minnesota Wild', 'Montreal Canadiens', 'Nashville Predators', 'New Jersey Devils', 'New Rork Islanders', 'New York Rangers', 'Ottawa Senators', 'Philadelphia Flyers', 'Phoenix Coyotes', 'Pittsburgh Penguins', 'Saint Louis Blues', 'San Jose Sharks', 'Tampa Bay Lightning', 'Toronto Maple Leafs', 'Vancouver Canucks', 'Washington Capitals'];
+// var nbaTeams = ['Atlanta Hawks', 'Boston Celtics', 'Charlotte Bobcats', 'Chicago Bulls', 'Cleveland Cavaliers', 'Dallas Mavericks', 'Denver Nuggets', 'Detroit Pistons', 'Golden State Warriors', 'Houston Rockets', 'Indiana Pacers', 'LA Clippers', 'LA Lakers', 'Memphis Grizzlies', 'Miami Heat', 'Milwaukee Bucks', 'Minnesota Timberwolves', 'New Jersey Nets', 'New Orleans Hornets', 'New York Knicks', 'Oklahoma City Thunder', 'Orlando Magic', 'Philadelphia Sixers', 'Phoenix Suns', 'Portland Trail Blazers', 'Sacramento Kings', 'San Antonio Spurs', 'Toronto Raptors', 'Utah Jazz', 'Washington Wizards'];
+// var nhl = $.map(nhlTeams, function (team) { return { value: team, data: { category: 'NHL' }}; });
+// var nba = $.map(nbaTeams, function (team) { return { value: team, data: { category: 'NBA' } }; });
+// var teams = nhl.concat(nba);
+//
+// // Initialize autocomplete with local lookup:
+// $('#gameInput').devbridgeAutocomplete({
+//     lookup: teams,
+//     minChars: 1,
+//     onSelect: function (suggestion) {
+//         $('#selection').html('You selected: ' + suggestion.value + ', ' + suggestion.data.category);
+//     },
+//     showNoSuggestionNotice: true,
+//     noSuggestionNotice: 'Sorry, no matching results',
+//     groupBy: 'category'
+// });
+
+// ====================================== DEMO END
 
 let gameArray = []
-var results = []
-$('#game').keydown(function(e){
-  if($(this).val().length >= 3 && e.which != 13) {
-    $.get({
-      url: '/LANker/public/autocomplete',
-      data: {name:$('#game').val()},
-      success: function (data, textStatus, jqXHR) {
-        console.log(data);
-        let json_data = JSON.parse(data)
-        var game_names = json_data.map(item => item['name'])
-        results = game_names
-        console.log(game_names);
-        $('#game').autocomplete({
-          source: game_names
-        })
-      }
-    });
-  }
-  if (e.which == 13) {
-    if (results.includes($(this).val()) && !gameArray.includes($(this).val())) {
-      let id = $(this).val()
-      gameArray.push(id)
+// var results = []
+
+// Initialize ajax autocomplete: WILL NOT WORK BECAUSE FORMAT IS NOT THE SAME AS APIs
+// SEE : https://www.devbridge.com/sourcery/components/jquery-autocomplete/#jquery-autocomplete-response-format
+$('#gameInput').autocomplete({
+  paramName: 'name',
+  transformResult: function(response) {
+    // console.log(response);
+    let gameData = JSON.parse(response)
+    // console.log(gameData);
+    let gameObjectArray = []
+
+    gameData.forEach(dataItem => gameObjectArray.push({value:dataItem.name, data:dataItem.id})) // TODO : regler ça puis donner le data a l'autocomplete
+    // console.log({suggestions:gameObjectArray});
+
+    /*
+    * Required format by devbridge autocomplete :
+    * { suggestions:
+    *   [
+    *   {value:"Value1", data: 1},
+    *   {value:"Value2", data: 2},
+    *   {value:"Value3", data: 3},
+    *   ...
+    *   ]
+    * }
+    */
+    return {suggestions:gameObjectArray}
+  },
+  serviceUrl: '/LANker/public/autocomplete',
+  // lookupFilter: function(suggestion, originalQuery, queryLowerCase) {
+  //   var re = new RegExp('\\b' + $.Autocomplete.utils.escapeRegExChars(queryLowerCase), 'gi');
+  //   return re.test(suggestion.value);
+  // },
+  onSelect: function(suggestion) {
+    let id = suggestion.data
+    let name = suggestion.value
+    if(!gameArray.includes(name)) {
+      gameArray.push(name)
+
       let tag = $('<p>', {
         id: id,
-        "class": 'lanker-tag',
-        text: id,
+        "class": 'lanker-tag btn btn-primary mr-2',
+        text: name,
         click: function() {
-          let gameIndex = gameArray.indexOf(tag.attr('id'));
+          let gameIndex = gameArray.indexOf(tag.attr('name'));
           tag.remove()
           gameArray.splice(gameIndex, 1)
-          console.log("removed");
-
+          createHiddenInputWithGames(gameArray)
         }
       })
+
+      createHiddenInputWithGames(gameArray)
       $('#game_tags').append(tag)
     }
+    $('#gameInput').val('')
   }
-})
+  // onHint: function (hint) {
+  //   $('#autocomplete-ajax-x').val(hint);
+  // },
+  // onInvalidateSelection: function() {
+  //   $('#selction-ajax').html('You selected: none');
+  // }
+});
+
+function createHiddenInputWithGames(gameArray) {
+  if($('#hiddenGames').length) {
+    $('#hiddenGames').remove()
+  }
+
+  let hiddenGames = $('<input>', {
+    type: 'hidden',
+    name: 'frmHidden',
+    id: 'hiddenGames',
+    value: gameArray
+  })
+
+  $('#eventForm').append(hiddenGames)
+}
--- resources/views/event_form.blade.php
+++ resources/views/event_form.blade.php
@@ -1,116 +1,113 @@
 @extends('layouts.app')
 
 @section('content')
-<script src="{{ asset('js/image_preview.js') }}" defer></script>
-
 <div class="container">
-  <div class="container">
-    <h1 class="display-4">Create Event</h1>
-    <div class="card">
-      <form class="card-body form-col ml-2 mr-2" enctype="multipart/form-data" method="POST" action="{{ route('create_event') }}">
-          @csrf
+  <h1 class="display-4">Create Event</h1>
+  <div class="card">
+    <form id="eventForm" class="card-body form-col ml-2 mr-2" enctype="multipart/form-data" method="POST" action="{{ route('create_event') }}">
+        @csrf
 
-          <div class="form-group row">
-            <label for="name">Event name</label>
-            <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" placeholder="Name" required>
-            @error('name')
-                <span class="invalid-feedback" role="alert">
-                    <strong>{{ $message }}</strong>
-                </span>
-            @enderror
-          </div>
+        <div class="form-group row">
+          <label for="name">Event name</label>
+          <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" placeholder="Name" required>
+          @error('name')
+              <span class="invalid-feedback" role="alert">
+                  <strong>{{ $message }}</strong>
+              </span>
+          @enderror
+        </div>
 
-          <div class="form-group row">
-            <div class="form-row">
-              <div class="col-xs-9">
-                <img src="{{ url('storage/banners/dreamhack.jpg')}}" id="image_preview_container" class="img-thumbnail float-left event-banner" alt="">
-              </div>
-              <div class="col-xs-6 align-self-end mt-2 @error('image') is-invalid @enderror">
-                <span class="btn btn-primary btn-file">
-                  @error('image')
-                      <span class="invalid-feedback" role="alert">
-                          <strong>{{ $message }}</strong>
-                      </span>
-                  @enderror
-                  Browse an image <input type="file" id="image" name="image" class="form-control" accept="image/*"/>
-                </span>
-              </div>
+        <div class="form-group row">
+          <div class="form-row">
+            <div class="col-xs-9">
+              <img src="{{ url('storage/banners/dreamhack.jpg')}}" id="image_preview_container" class="img-thumbnail float-left event-banner" alt="">
+            </div>
+            <div class="col-xs-6 align-self-end mt-2 @error('image') is-invalid @enderror">
+              <span class="btn btn-primary btn-file">
+                @error('image')
+                    <span class="invalid-feedback" role="alert">
+                        <strong>{{ $message }}</strong>
+                    </span>
+                @enderror
+                Browse an image <input type="file" id="image" name="image" class="form-control" accept="image/*"/>
+              </span>
             </div>
           </div>
+        </div>
 
-          <div class="form-group row">
-            <div class="form-row">
-              <div class="col-xs-6  mr-4">
-                <label for="date">Date</label>
-                <input id="date" type="date" class="form-control @error('date') is-invalid @enderror" name="date" value="{{ old('date') }}" placeholder="DD-MM-YYYY" pattern="[0-9]{2}-[0-9]{2}-[0-9]{4}" required>
-                @error('date')
+        <div class="form-group row">
+          <div class="form-row">
+            <div class="col-xs-6  mr-4">
+              <label for="date">Date</label>
+              <input id="date" type="date" class="form-control @error('date') is-invalid @enderror" name="date" value="{{ old('date') }}" placeholder="DD-MM-YYYY" pattern="[0-9]{2}-[0-9]{2}-[0-9]{4}" required>
+              @error('date')
+                  <span class="invalid-feedback" role="alert">
+                      <strong>{{ $message }}</strong>
+                  </span>
+              @enderror
+            </div>
+            <div class="col-xs-6">
+              <label for="start">Time</label>
+              <div class="form-row">
+                <label class="col-0 align-self-center" for="start" >From</label>
+                <input id="start" type="text" class="form-control col @error('start') is-invalid @enderror" name="start" value="{{ old('start') }}" placeholder="HH:MM" required>
+                @error('start')
+                    <span class="invalid-feedback" role="alert">
+                        <strong>{{ $message }}</strong>
+                    </span>
+                @enderror
+                <label class="col-0 align-self-center" for="end" >To</label>
+                <input id="end" type="text" class="form-control col @error('end') is-invalid @enderror" name="end" value="{{ old('end') }}" placeholder="HH:MM" required>
+                @error('end')
                     <span class="invalid-feedback" role="alert">
                         <strong>{{ $message }}</strong>
                     </span>
                 @enderror
               </div>
-              <div class="col-xs-6">
-                <label for="start">Time</label>
-                <div class="form-row">
-                  <label class="col-0 align-self-center" for="start" >From</label>
-                  <input id="start" type="text" class="form-control col @error('start') is-invalid @enderror" name="start" value="{{ old('start') }}" placeholder="HH:MM" required>
-                  @error('start')
-                      <span class="invalid-feedback" role="alert">
-                          <strong>{{ $message }}</strong>
-                      </span>
-                  @enderror
-                  <label class="col-0 align-self-center" for="end" >To</label>
-                  <input id="end" type="text" class="form-control col @error('end') is-invalid @enderror" name="end" value="{{ old('end') }}" placeholder="HH:MM" required>
-                  @error('end')
-                      <span class="invalid-feedback" role="alert">
-                          <strong>{{ $message }}</strong>
-                      </span>
-                  @enderror
-                </div>
-              </div>
             </div>
           </div>
+        </div>
 
-          <div class="form-group row">
-            <label for="location">Location</label>
-            <input id="location" type="text" class="form-control @error('location') is-invalid @enderror" name="location" value="{{ old('location') }}" placeholder="Location" required>
-            @error('location')
-                <span class="invalid-feedback" role="alert">
-                    <strong>{{ $message }}</strong>
-                </span>
-            @enderror
-          </div>
+        <div class="form-group row">
+          <label for="location">Location</label>
+          <input id="location" type="text" class="form-control @error('location') is-invalid @enderror" name="location" value="{{ old('location') }}" placeholder="Location" required>
+          @error('location')
+              <span class="invalid-feedback" role="alert">
+                  <strong>{{ $message }}</strong>
+              </span>
+          @enderror
+        </div>
+        <div class="form-group row">
+          <label for="game">Game</label>
+          <input id="gameInput" type="text" class="form-control @error('game') is-invalid @enderror"  name="game" value="{{ old('game') }}" placeholder="Games" required>
+          @error('game')
+              <span class="invalid-feedback" role="alert">
+                  <strong>{{ $message }}</strong>
+              </span>
+          @enderror
+        </div>
 
-          <div class="form-group row">
-            <label for="game">Game</label>
-            <div id="game_tags">
+        <div class="form-group row d-block">
+          <label for="">Selected games</label>
+          <span id="game_tags" class="d-block">
+          </span>
+        </div>
 
-            </div>
-            <input id="game" type="text" class="form-control @error('game') is-invalid @enderror"  name="game" value="{{ old('game') }}" placeholder="Games" required>
-            @error('game')
-                <span class="invalid-feedback" role="alert">
-                    <strong>{{ $message }}</strong>
-                </span>
-            @enderror
-          </div>
+        <div class="form-group row">
+          <label for="description">Description</label>
+          <textarea id="description" class="form-control  @error('description') is-invalid @enderror" name="description" value="{{ old('description') }}"></textarea>
+          @error('description')
+              <span class="invalid-feedback" role="alert">
+                  <strong>{{ $message }}</strong>
+              </span>
+          @enderror
+        </div>
 
-          <div class="form-group row">
-            <label for="description">Description</label>
-            <textarea id="description" class="form-control  @error('description') is-invalid @enderror" name="description" value="{{ old('description') }}"></textarea>
-            @error('description')
-                <span class="invalid-feedback" role="alert">
-                    <strong>{{ $message }}</strong>
-                </span>
-            @enderror
-          </div>
-
-          <div class="row">
-            <button class="btn btn-primary mr-2" type="submit" >Create event</button>
-            <a class="btn btn-secondary" href="{{ url()->previous() }}">Cancel</a>
-          </div>
-
-        </form>
-    </div>
+        <div class="row">
+          <button class="btn btn-primary mr-2" type="submit" >Create event</button>
+          <a class="btn btn-secondary" href="{{ url()->previous() }}">Cancel</a>
+        </div>
+    </form>
   </div>
 </div>
 @endsection
