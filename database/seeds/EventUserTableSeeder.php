dd540faf80c0cdce12662a69f08835511cb41f46
Minnig Nicolas <nicolas.minnig@he-arc.ch>
2019-12-12 13:49:13 +0100

    added check for duplicates in participation seeder (EventGameTableSeeder)

--- database/seeds/EventUserTableSeeder.php
+++ database/seeds/EventUserTableSeeder.php
@@ -14,10 +14,16 @@ class EventUserTableSeeder extends Seeder
      */
     public function run()
     {
-        for ($i=0; $i < 20; $i++) {
-          $event_id = Event::select('id')->inRandomOrder()->first();
-          $user_id = User::select('id')->inRandomOrder()->first();
-          Eventuser::create(['event_id'=>$event_id['id'],'user_id'=>$user_id['id']]);
+        $nb_insert=0;
+        while ($nb_insert < 30) {
+          $event_id = Event::select('id')->inRandomOrder()->first(); //get random event id
+          $user_id = User::select('id')->inRandomOrder()->first(); //get random user id
+          $event_id=$event_id['id'];
+          $user_id=$user_id['id'];
+          if(Eventuser::where('event_id',$event_id)->where('user_id',$user_id)->count()==0){ //if not already in database, insert it
+            Eventuser::create(['event_id'=>$event_id,'user_id'=>$user_id]);
+            $nb_insert++;
+          }
         }
     }
 }
