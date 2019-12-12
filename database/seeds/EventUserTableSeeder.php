c447e9ab1fa4bfd5a7163e1e18e0dbe72fae9f2a
Minnig Nicolas <nicolas.minnig@he-arc.ch>
2019-12-12 14:01:04 +0100

    added limitation of iterations when adding participations

--- database/seeds/EventUserTableSeeder.php
+++ database/seeds/EventUserTableSeeder.php
@@ -14,16 +14,19 @@ class EventUserTableSeeder extends Seeder
      */
     public function run()
     {
-        $nb_insert=0;
-        while ($nb_insert < 30) {
+        $nb_insert=30;
+        $i_insert=0;
+        $i_iterations=0;
+        while ($i_insert < 30 && $i_iterations<5*$nb_insert) {
           $event_id = Event::select('id')->inRandomOrder()->first(); //get random event id
           $user_id = User::select('id')->inRandomOrder()->first(); //get random user id
           $event_id=$event_id['id'];
           $user_id=$user_id['id'];
           if(Eventuser::where('event_id',$event_id)->where('user_id',$user_id)->count()==0){ //if not already in database, insert it
             Eventuser::create(['event_id'=>$event_id,'user_id'=>$user_id]);
-            $nb_insert++;
+            $i_insert++;
           }
+          $i_iterations++;
         }
     }
 }
