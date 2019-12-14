dcc13d29f9a4f110b1b9be73d3d6638637b2925d
Minnig Nicolas <nicolas.minnig@he-arc.ch>
2019-12-14 16:57:44 +0100

    fix game display on low width + added past events in seeder

--- resources/views/components/game_card.blade.php
+++ resources/views/components/game_card.blade.php
@@ -1,6 +1,6 @@
 
 
-<div class="card lanker-img-card mx-4 my-2" >
+<div class="card col-md-3 mx-4 my-2" > 
   <img src="{{$cover}}" class="card-img-top lanker-img-card-img " alt="...">
   <div class="card-body">
     <h5 class="card-text">{{$title}}</h5>
