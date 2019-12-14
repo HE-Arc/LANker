6c4a094569bdb44aaebdac7f972bc00217e0d869
artan.sadiku <artan.sadiku@he-arc.ch>
2019-12-14 19:56:42 +0100

    Updated profile design

--- resources/views/components/profile_card.blade.php
+++ resources/views/components/profile_card.blade.php
@@ -1,15 +1,11 @@
-<div class="card mb-3 profile_card">
+<div class="card mb-3 p-2">
   <div class="row no-gutters">
-    <div class="col-md-4">
-      <img src="{{ url('storage/'.$avatar) }}" class="card-img" alt="...">
+    <div class="col-md-2">
+      <img src="{{ url('storage/'.$avatar) }}" class="card-img lanker-profile-card" alt="...">
     </div>
-    <div class="col-md-8">
-      <div class="card-body">
-        <h1 class="card-title">{{ $name }}</h5>
-        <p>{{ $email }}</p>
-        <p class="card-text formatted">{!! $description !!}</p>
-        <p>Joined on {{ date("d.m.Y",strtotime($created_at)) }}</p>
-        <a class="btn btn-primary" href="{{ route('profile', $name) }}">Visit Profile</a>
+    <div class="col-md-8 my-auto">
+      <div class="card-body p-0">
+        <h4 class="card-title my-auto"><a href="{{ route('profile', $name)}}">{{ $name }}</a></h4>
       </div>
     </div>
   </div>
